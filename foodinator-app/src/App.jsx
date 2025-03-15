5431d1b7471bc656e0e395b2deb6913df0ad6f84
Marcus Chow <mcho0083@student.monash.edu>
2025-03-15 23:43:08 +1100

    Moved Initial page to separate ConfigPage component

--- foodinator-app/src/App.jsx
+++ foodinator-app/src/App.jsx
@@ -1,24 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './styles/global.css';
 import './App.css';
-import axios from 'axios';
-import RadiusSlider from './components/RadiusSlider';
+
+import ConfigPage from './components/ConfigPage'
+import Question from './components/Question'
+
 
 
 function App() {
   // State to track the current page/content
   const [currentPage, setCurrentPage] = useState('home'); // Can be 'home' or 'question'
-
-  // State to track the current location
-  const [location, setLocation] = useState({ latitude: null, longitude: null });
-  const [error, setError] = useState(null);
-
-  // State to track post code and coordinate  
-  const API_KEYS = {
-    "maps": `${process.env.REACT_APP_MAPS_API}`
-  }
-  const [postcode, setPostcode] = useState(null);
-  const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
 
 
   /**
@@ -54,52 +45,11 @@ function App() {
     setCurrentPage('question'); // Move from home to the question page
   };
 
-  const handleConvert = async () => {
-    console.log(typeof(API_KEY))
-    try {
-      // Make a request to the Geocoding API
-      const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${postcode},AU&key=${API_KEYS.maps}`
-      );
-
-      // Extract latitude and longitude from the response
-      const { lat, lng } = response.data.results[0].geometry.location;
-
-      // Update state with the coordinates
-      setCoordinates({ lat, lng });
-      setError(null);
-    } catch (err) {
-      setError('Failed to convert postcode to coordinates. Please try again.');
-      console.error(err);
-    }
-  };
-
-  // Gets the users current location
-  const getLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-          setError(null);
-        },
-        (err) => {
-          setError(err.message);
-        }
-      );
-    } else {
-      setError('Geolocation is not supported by this browser.');
-    }
-  
-  };
-
-  const [selectedRadius, setSelectedRadius] = useState(10); // Default radius: 10 km
-
-  const handleRadiusChange = (newRadius) => {
-    setSelectedRadius(newRadius);
-  };
+   // Handle moving to the prompts page
+   const handleStartConvo = () => {
+    setCurrentPage("test"); // Move from home to the question page
+    
+};
 
   return (
     <div className="app">
@@ -112,42 +62,9 @@ function App() {
           </div>
         </div>
       )}
-      
-      {currentPage === 'question' && (
-        <div className="home-prompt">
-          <button onClick={getLocation}>Use your current location.</button>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-          {location.latitude && location.longitude && (
-            <div>
-              <p>Latitude: {location.latitude}</p>
-              <p>Longitude: {location.longitude}</p>
-            </div>
-          )}
-          <div>
-          <p className="question">* Or tell us your postcode:</p>
-            <input
-              type="text"
-              placeholder="Enter Victorian postcode"
-              value={postcode}
-              onChange={(e) => setPostcode(e.target.value)}
-            />
-            <button onClick={handleConvert}>Convert</button>
+      {currentPage === 'question' && <ConfigPage  nextStageFunction={handleStartConvo}/>}
+      {currentPage === 'test' && questionData && <Question  question={questionData.question} options={questionData.options}/>}
 
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-
-            {coordinates.lat && coordinates.lng && (
-              <div>
-                <p>Latitude: {coordinates.lat}</p>
-                <p>Longitude: {coordinates.lng}</p>
-              </div>
-            )}
-            
-          </div>
-          <div className="app">
-            <RadiusSlider onRadiusChange={handleRadiusChange} />
-          </div>
-        </div>
-      )}
     </div>
   );
 }
