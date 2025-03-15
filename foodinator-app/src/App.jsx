4d3789effe9d26dbc8bcd4d34f83d7b9d84717f7
Arosh Heenkenda <ahee0011@student.monash.edu>
2025-03-15 16:07:55 +1100

    Try figure out how to pull data from server.

--- foodinator-app/src/App.jsx
+++ foodinator-app/src/App.jsx
@@ -5,8 +5,35 @@ import './App.css';
 function App() {
   // State to track the current page/content
   const [currentPage, setCurrentPage] = useState('home'); // Can be 'home' or 'question'
-
   const [currentLocation, setLocation] = useState('no');
+
+  /**
+   * Data relating to question
+   * 
+   * JSON Format:
+   * {
+   *  question: ...
+   *  options: []
+   * }
+   * 
+   */
+  const [questionData, setQuestionData] = useState(null); //
+
+  /**
+   * UseEffect to get question data from API
+   */
+  useEffect(()=>{
+    fetch('http://localhost:5000/example',{
+      'methods':'GET',
+      headers : {
+        'Content-Type':'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(response => setQuestionData(response))
+    .catch(error => console.log(error))
+
+  },[]) //in '[]' we can set when we make this get request?
 
   // Handle moving to the next page
   const handleNext = () => {
