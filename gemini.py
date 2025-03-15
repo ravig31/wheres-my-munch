887a9fe3d3a772a58d171b4c5344a5b87e51d4a9
ravindugamage <rgam0005@student.monash.edu>
2025-03-15 11:32:41 +1100

    fix gitignore

--- gemini.py
+++ gemini.py
@@ -1,15 +1,19 @@
 from google import genai
+from google.genai import types
 
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
-GEMINI_TOKEN = getenv("GEMINI_API_TOKEN")
-client = genai.Client(api_key="YOUR_API_KEY")
+GEMINI_TOKEN = getenv("GEMINI_API_KEY")
+
+
+sys_instruct=""
+client = genai.Client(api_key="GEMINI_API_KEY")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="Explain how AI works",
+    config=types.GenerateContentConfig(
+        system_instruction=sys_instruct),
+    contents=["your prompt here"]
 )
-
-print(response.text)
