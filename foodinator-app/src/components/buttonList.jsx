597c3ef10563d9ebde8907a1eac21d09139f98f2
Arosh Heenkenda <ahee0011@student.monash.edu>
2025-03-15 17:45:24 +1100

    Try to get display to match desired.

--- foodinator-app/src/components/buttonList.jsx
+++ foodinator-app/src/components/buttonList.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 const ButtonList = ({ items }) => {
-
     return (
-        <div className="button-list">
+        <div className="question-buttons-list">
             {items.map((item, index) => (
                 <button key={index} className="dynamic-button">
                     {item}
