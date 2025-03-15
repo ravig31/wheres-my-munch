25ad428767b5fc47c84185bc0c770651b71d7ee4
Arosh Heenkenda <ahee0011@student.monash.edu>
2025-03-15 12:48:51 +1100

    Base changes, add button and title.

--- foodinator-app/src/App.js
+++ foodinator-app/src/App.js
@@ -4,19 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="foodinator-body">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Foodinie
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          Hungry?
+        </button>
       </header>
     </div>
   );
