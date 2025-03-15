e3801521b3a5d61d12d0899014b637bad5b3129b
Arosh Heenkenda <ahee0011@student.monash.edu>
2025-03-15 19:08:25 +1100

    Add a POST request for when the button is clicked.

--- foodinator-app/src/components/buttonList.jsx
+++ foodinator-app/src/components/buttonList.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
 const ButtonList = ({ items }) => {
+
+    // Function to handle button click and send data to Flask
+    // TODO: Change and test the dummy request
+    const handleClick = (option) => {
+        fetch("http://localhost:5000/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ selectedOption: option }),
+        })
+            .then(response => response.json())
+            .then(data => console.log("Response from server:", data))
+            .catch(error => console.error("Error:", error));
+    };
+
     return (
         <div className="question-buttons-list">
             {items.map((item, index) => (
-                <button key={index} className="dynamic-button">
+                <button key={index} className="dynamic-button" onClick={() => handleClick(item)}>
                     {item}
                 </button>
             ))}
